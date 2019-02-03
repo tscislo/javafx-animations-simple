7eb56eff49567bcd679dcf80e281e04fc8d039e9
Tomasz Ścisło <scislot@gmail.com>
2019-02-03 10:55:19 +0100

    pavkages

---
+++ src/eu/scislo/mobilenext/controller/AnimationWindowController.java
@@ -0,0 +1,18 @@
+package eu.scislo.mobilenext.controller;
+
+import javafx.stage.Stage;
+
+public class AnimationWindowController {
+
+    private Stage animationWindowStage;
+    private int animationNo;
+
+    public void setAnimationWindowStage(Stage animationWindowStage) {
+        this.animationWindowStage = animationWindowStage;
+    }
+
+
+    public void setAnimationNo(int animationNo) {
+        this.animationNo = animationNo;
+    }
+}
