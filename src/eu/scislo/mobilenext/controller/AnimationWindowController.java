083b8677a986051a4fd45eb177d2fb74625397e7
Tomasz Ścisło <scislot@gmail.com>
2019-02-03 11:49:33 +0100

    basic rotate animation

--- src/eu/scislo/mobilenext/controller/AnimationWindowController.java
+++ src/eu/scislo/mobilenext/controller/AnimationWindowController.java
@@ -1,14 +1,18 @@
 package eu.scislo.mobilenext.controller;
 
+import javafx.animation.Animation;
+import javafx.animation.RotateTransition;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class AnimationWindowController {
 
     private Stage animationWindowStage;
     private int animationNo;
+    private Animation animation;
 
     @FXML
     Rectangle rect1;
@@ -27,6 +31,7 @@ public class AnimationWindowController {
 
     public void setAnimationNo(int animationNo) {
         this.animationNo = animationNo;
+        this.prepareAnimation();
     }
 
     @FXML
@@ -36,17 +41,29 @@ public class AnimationWindowController {
 
     @FXML
     public void play() {
-
+        animation.play();
     }
 
     @FXML
     public void pause() {
-
+        animation.pause();
     }
 
     @FXML
     public void stop() {
+        animation.stop();
+    }
 
+    private void prepareAnimation() {
+        if (animationNo == 1) {
+            RotateTransition transition = new RotateTransition();
+            transition.setNode(this.rect1);
+            transition.setDuration(Duration.seconds(10));
+            transition.setFromAngle(0);
+            transition.setToAngle(360);
+            transition.setCycleCount(Animation.INDEFINITE);
+            animation = transition;
+        }
     }
 
 }
